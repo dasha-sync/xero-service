e9eeb7667b6651b56edd9487bd17e44d629d8e97
Daria Kapoor <120682364+DariaAres@users.noreply.github.com>
2023-11-13 15:23:13 +0300

    Add user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    username { Faker::Internet.username(specifier: 10) }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 6, max_length: 20) }
+  end
+end
