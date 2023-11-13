be76d8a4800366901d993299c969b2a770c91b25
daria <kapoor.darya@gmai.com>
2023-11-13 17:53:22 +0300

    Add user controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+
+  protected
+
+  def configure_sign_up_params
+    attributes = %i[first_name last_name email password password password_confirmation]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
 end
