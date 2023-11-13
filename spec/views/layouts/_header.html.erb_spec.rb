e57df420a666ad58bbe7fed3762c34e8f265d4d4
daria <kapoor.darya@gmai.com>
2023-11-13 17:40:23 +0300

    Add tests

---
+++ spec/views/layouts/_header.html.erb_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'layouts/_header.html.erb', type: :view do
+  describe 'Home route path', type: :routing do
+    before { render template: 'layouts/application' }
+
+    it 'include header text' do
+      expect(rendered).to include('button', 'Home')
+    end
+  end
+end
