08a642dfc309a41c9b30365fb1ab518a0cb535b6
daria <kapoor.darya@gmai.com>
2023-11-14 13:20:56 +0300

    Add user controllers

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  # POST /resource
+  def create(*args, **kwargs)
+    super
+  end
+
+  def update(*args, **kwargs)
+    super
+  end
+
+  private
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
